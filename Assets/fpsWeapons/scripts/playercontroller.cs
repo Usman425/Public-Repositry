04540708a5955aa90154c69d75c863b987e4519f
Huzaifa Ahmad <huzaifa.ahmad55@gmail.com>
2021-08-06 15:33:45 +0500

    gg

--- Assets/fpsWeapons/scripts/playercontroller.cs
+++ Assets/fpsWeapons/scripts/playercontroller.cs
@@ -113,7 +113,7 @@ public class playercontroller : MonoBehaviour {
 	private void Start ()
 	{
 		speed = normalspeed;
-		painflashtexture.CrossFadeAlpha(0f,0f,true);
+		painflashtexture.CrossFadeAlpha(5f,0f,true);
 		cameranextposition = camerahighposition;
 
 	}
